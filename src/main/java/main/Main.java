ac2fd6ff5f13db2240d76b4f93118f69d99f5d8b
LazarevY <lazarev@cs.vsu.ru>
2020-11-03 14:10:58 +0300

    Add ui run

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,24 @@
 package main;
 
 
+import core.dao.DAO;
+import core.dao.SQLBasedDAO;
+import core.inverseofcontrol.boot.Application;
+import core.inverseofcontrol.boot.ApplicationContext;
+import ui.UI;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        ApplicationContext context = Application.run("",
+                new HashMap<>(Map.of(DAO.class, SQLBasedDAO.class)));
+
+        UI ui = context.getObject(UI.class);
+        ui.show();
 
     }
 }
